596ae9356737fa724ed0d886022ec8c600223611
marcewp15 <970@holbertonschool.com>
2019-09-19 23:07:37 +0000

    Fix betty style, This program prints all the numbers of base 16 in lowercase with loops and putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 	int numb;
 	char alphab;
 
-	for(numb = 48 ; numb <= 57 ; numb++)
+	for (numb = 48 ; numb <= 57 ; numb++)
 		putchar(numb);
-	for(alphab = 97 ; alphab <= 102 ; alphab++)
+	for (alphab = 97 ; alphab <= 102 ; alphab++)
 		putchar(alphab);
 	putchar('\n');
 	return (0);
