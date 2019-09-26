1c90410c37d7859d70616b820a2322fb706fa834
marcewp15 <970@holbertonschool.com>
2019-09-26 15:31:11 +0000

    This function checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for a digit
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c <= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
