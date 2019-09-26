424940906d2a19e42d7f107af3102564065a2ffa
marcewp15 <970@holbertonschool.com>
2019-09-26 18:32:03 +0000

    Fix the style of the code (betty style), This function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  * _isupper - checks for uppercase character
+ * @c: the character to print
  *
  * Return: 1 or 0
  */
 int _isupper(int c)
 {
-	if (c <= 65 && c <= 90)
+	if ((c <= 65) && (c <= 90))
 	{
 		return (1);
 	}
