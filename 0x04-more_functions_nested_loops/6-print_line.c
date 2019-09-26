99cf8fc222277eee0c128bf7977638b8b93ae832
marcewp15 <970@holbertonschool.com>
2019-09-26 21:09:08 +0000

    Fix inicialization of parameters, This function draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
 /**
- * print_line -drws a straight line in the terminal
+ * print_line -draws a straight line in the terminal
  * @n: the number of times that the special character must be printed
  *
  * Return: a straight line
  */
 void print_line(int n)
 {
-	int a;
-
-	a = 0;
+	int a = 1;
 
 	while (a <= n)
 	{
 		_putchar(95);
 		a++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
