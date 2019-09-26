37003ca3654f466fe746c03d84bb48cecd12b0c4
marcewp15 <970@holbertonschool.com>
2019-09-26 20:48:38 +0000

    Fix style of the code (betty style), This function draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
  * print_line -drws a straight line in the terminal
+ * @n: the number of times that the special character must be printed
  *
- *
+ * Return: a straight line
  */
 void print_line(int n)
 {
