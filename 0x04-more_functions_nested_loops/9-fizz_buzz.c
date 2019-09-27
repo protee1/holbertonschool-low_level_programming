8e1c0700e46b6b0ae89d507ea7224331cdbb7186
marcewp15 <970@holbertonschool.com>
2019-09-27 00:20:22 +0000

    Fizz-Buzz test, this program prints the numbers from 0 to 100 and replace for Fizz or Buzz according to the multiples of three or five

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 1 ; m <= 100 ; m++)
+	{
+		if ((m % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((m % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((m % 5) == 0 && m == 100)
+		{
+			printf("Buzz");
+		}
+		else if ((m % 3) == 0 && (m % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+		printf("%d ", m);
+		}
+	}
+	printf("\n");
+	return (0);
+}
