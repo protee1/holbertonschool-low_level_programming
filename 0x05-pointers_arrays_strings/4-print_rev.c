04e896871ccfbcad2b0aada980dc5d4031dccd79
marcewp15 <970@holbertonschool.com>
2019-10-01 16:17:32 +0000

    Fix the style of the code (betty style), modification of the conditions of the code, This function prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -18,17 +18,3 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
-/**
- * _strlen - give the length of a string
- * @s: the string
- *
- * Return: the length of a string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0 ; s[i] != '\0' ; i++)
-		;
-	return (i);
-}
