8ecda5b2ba39dcbde58d3a3049c23721bca3414d
marcewp15 <970@holbertonschool.com>
2019-10-04 00:44:05 +0000

    Fix the style of the code (betty style),This function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
  * cap_string - capitalizes all words of a string
  * @str: the string to change the first letter of a word in uppercase
  *
+ * Return: capitalizes letters
  */
 char *cap_string(char *str)
 {
@@ -12,13 +13,13 @@ char *cap_string(char *str)
 
 	i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		j = 0;
 		while (c[j] != '\0')
 		{
 
-			if(c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
