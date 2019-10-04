7331128fba9f6891957808f83a12123b5535d96e
marcewp15 <970@holbertonschool.com>
2019-10-04 01:36:42 +0000

    Fix the conditions to print,This function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if(i == 0 && str[i] >= 97 && str[i] <= 122)
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
