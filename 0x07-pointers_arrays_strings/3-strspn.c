1d979a05b91f925797369b59a09d11efff65c3c5
marcewp15 <970@holbertonschool.com>
2019-10-07 21:30:09 +0000

    Modification of conditions in the code, This function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,10 +9,11 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i = 0;
-	int j = 0;
+	int j;
 
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
