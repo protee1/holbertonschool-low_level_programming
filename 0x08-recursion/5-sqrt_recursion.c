13a1c1297fe6419991d99dc686a51f2d9f936186
marcewp15 <970@holbertonschool.com>
2019-10-09 23:28:16 +0000

    Add description in the documentation, This function returns the natural square root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 /**
  * square_root - find the natural square root
+ * @i: number
+ * @j: incrementor
  *
- *
+ * Return: the number found
  */
 int square_root(int i, int j)
 {
 	if (i * i == j)
 	{
-		return(i);
+		return (i);
 	}
 	else if (i * i > j)
 	{
@@ -21,12 +23,12 @@ int square_root(int i, int j)
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
- *
- *
+ * @n: number
+ * Return: the natural square root found
  */
 int _sqrt_recursion(int n)
 {
 	int a = 1;
 
-	return(square_root(a, n));
+	return (square_root(a, n));
 }
