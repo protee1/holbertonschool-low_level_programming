be9dba4ec35075af0b4f3dffa23a39dc4f964cef
marcewp15 <970@holbertonschool.com>
2019-10-10 00:29:49 +0000

    Add the prototype to the function that shows the prime number, return 1 if its prime, otherwise return 0

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -7,4 +7,6 @@ int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
 int square_root(int i, int j);
 int _sqrt_recursion(int n);
+int prime_found(int i, int j);
+int is_prime_number(int n);
 #endif
