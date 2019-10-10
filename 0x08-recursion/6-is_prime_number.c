9926fd5e5a27e13c8dfe112cb878085a735e7ec0
marcewp15 <970@holbertonschool.com>
2019-10-10 00:29:27 +0000

    This function shows the prime number, return 1 if its prime, otherwise return 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * prime_found - found prime number
+ *
+ *
+ */
+int prime_found(int i, int j)
+{
+	if (j != 1)
+	{
+		if (i == j)
+		{
+			return (1);
+		}
+		else if (j % i == 0 || j <= 1)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime_found(i + 1, j));
+		}
+	}
+	return (0);
+}
+/**
+ * is_prime_number - shows if is a prime number
+ *
+ *
+ *
+ */
+int is_prime_number(int n)
+{
+	int a = 2;
+
+	return (prime_found(a, n));
+}
