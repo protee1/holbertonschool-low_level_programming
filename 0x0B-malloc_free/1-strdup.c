9e56fb98e1cb5188023273a6d875054d958fe221
marcewp15 <970@holbertonschool.com>
2019-10-15 21:08:44 +0000

    Fix style of the code (betty style), This function returns a pointer to newly allocated space in memory, wih contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ int _strlen(char *s)
 /**
  * _strdup - returns a pointer which contains a copy of the string
  * given as a parameter
- * @str: 
+ * @str: The string to print
  *
  *
  * Return: a pointer to the duplicated string or NULL if insufficient
@@ -32,21 +32,21 @@ char *_strdup(char *str)
 
 	if (str == '\0')
 	{
-		return('\0');
+		return ('\0');
 	}
 	i = _strlen(str);
 	copy = malloc(sizeof(char) * i + 1);
-	if(copy == '\0')
+	if (copy == '\0')
 	{
 		return ('\0');
 	}
 	else
 	{
-		for(i = 0 ; str[i] != '\0' ; i++)
+		for (i = 0 ; str[i] != '\0' ; i++)
 		{
 			copy[i] = str[i];
 		}
 	}
 	copy[i] = '\0';
-	return(copy);
+	return (copy);
 }
