c7a4a595678b80ff8ec9ac766b8f509288255772
marcewp15 <970@holbertonschool.com>
2019-10-17 21:18:30 +0000

    Add the prototype to the function that concatenates two strings

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,4 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+unsigned int _strlen(char *s);
+char *_strcat(char *dest, char *src);
 #endif
