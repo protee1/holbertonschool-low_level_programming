2dbd11b74110a5febb20f9b214c1cec33eff1532
marcewp15 <970@holbertonschool.com>
2019-10-17 21:43:30 +0000

    Add the prototype to the function that allocates memory for an array using malloc

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,5 +3,5 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int _strlen(char *s);
-char *_strcat(char *dest, char *src);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
