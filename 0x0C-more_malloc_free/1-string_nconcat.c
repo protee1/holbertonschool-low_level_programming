77eab3d22e0c87253c9a33fccc63fbde5389eae8
marcewp15 <970@holbertonschool.com>
2019-10-17 21:21:35 +0000

    This function concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -51,16 +51,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	else
-	{
-		for (i = 0 ; i < length1 ; i++)
-		{
-			space[i] = s1[i];
-		}
-		for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
-		{
-			space[i] = s2[j];
-		}
-	}
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
 	return (space);
 }
