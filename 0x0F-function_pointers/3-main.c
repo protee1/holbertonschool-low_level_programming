32289dac96bdbfe7a025b856c79814940018f16a
marcewp15 <970@holbertonschool.com>
2019-10-24 05:25:29 +0000

    Fix conditions and variables in the code, This main contain the program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,8 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int (*operation)(int, int);
-	int num1, num2, total;
+	int operation, num1, num2;
 
 	if (argc != 4)
 	{
@@ -18,10 +17,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-
 	operation = get_op_func(argv[2])(num1, num2);
-	total = operation(argv[1], argv[2]);
-
-	printf("%d\n", total);
+	printf("%d\n", operation);
 	return (0);
 }
