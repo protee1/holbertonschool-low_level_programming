aa38a8c2b4cc6452df77289215f206989f15f0c8
marcewp15 <970@holbertonschool.com>
2019-10-24 22:47:14 +0000

    This function that returns the factorial of a given number with recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
  * factorial - find the factorial of a given number
  * @n: an argument passed to the function
  *
- * Return: the factorial of a number, -1 if lower than 0
+ * Return: the factorial of a given number, -1 if lower than 0
  */
 int factorial(int n)
 {
