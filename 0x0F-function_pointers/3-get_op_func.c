4724049d2a74dfe49047a20d0246df8c1df83b94
marcewp15 <970@holbertonschool.com>
2019-10-24 19:55:40 +0000

    Fix conditions and variables in the code, This main contain the program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *(s + 0) && *(s + 1) == '\0')
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
-	    argv[2][0] != '%' && argv[2][0] != '*')
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
+	     argv[2][0] != '%' && argv[2][0] != '*') || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
