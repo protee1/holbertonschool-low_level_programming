243eda2040eed97c43e864d6b747c0c00541e1d4
marcewp15 <970@holbertonschool.com>
2019-10-25 19:05:58 +0000

    Add the prototype to the function that prints numbers with variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
+#include <stdio.h>
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
