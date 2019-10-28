5670771358e51926eb62cce8aa4e82921778f4d3
marcewp15 <970@holbertonschool.com>
2019-10-28 03:17:34 +0000

    Add the prototype to the function that prints anything with variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,18 @@
 #define VARIADIC_FUNCTIONS
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ *
+ *
+ *
+ */
+typedef struct print_all
+{
+	char all;
+	void (*func)(va_list arguments);
+} id_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
