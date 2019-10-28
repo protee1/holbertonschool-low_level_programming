c26e4b9c2e7f63c3ce3792bc3c42f60dec1223bc
marcewp15 <970@holbertonschool.com>
2019-10-28 03:58:20 +0000

    Fix the condition and style of the code, modify the header file, This function that prints anything with variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,12 +13,9 @@ typedef struct print_all
 	char all;
 	void (*func)(va_list arguments);
 } simbol_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_int(va_list arguments);
-void print_string(va_list arguments);
-void print_char(va_list arguments);
-void print_float(va_list arguments);
 #endif
