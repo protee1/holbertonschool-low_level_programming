1378fad4f28ea58b9d07079f474f14ce84a480d6
marcewp15 <970@holbertonschool.com>
2019-10-28 03:29:19 +0000

    Add the prototypes to every function inside the function that prints anything with variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,4 +16,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_int(va_list arguments);
+void print_string(va_list arguments);
+void print_char(va_list arguments);
+void print_float(va_list arguments);
 #endif
