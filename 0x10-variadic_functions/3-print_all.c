336ad855d55beaa37cee7de80f888ab1f107bbd1
marcewp15 <970@holbertonschool.com>
2019-10-28 04:00:59 +0000

    Fix the condition and style of the code, modify the header file, This function that prints anything with variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,6 +8,22 @@ void print_char(va_list arguments)
 	printf("%c", va_arg(arguments, int));
 }
 /**
+ * print_int - print integers
+ * @arguments: input arguments
+ */
+void print_int(va_list arguments)
+{
+	printf("%d", va_arg(arguments, int));
+}
+/**
+ * print_float - print floats
+ * @arguments: input arguments
+ */
+void print_float(va_list arguments)
+{
+	printf("%f", va_arg(arguments, double));
+}
+/**
  * print_string - print strings
  * @arguments: input arguments
  */
@@ -20,22 +36,6 @@ void print_string(va_list arguments)
 		printf("%p", args);
 	}
 	printf("%s", args);
-}
-/**
- * print_float - print floats
- * @arguments: input arguments
- */
-void print_float(va_list arguments)
-{
-	printf("%f", va_arg(arguments, double));
-}
-/**
- * print_int - print integers
- * @arguments: input arguments
- */
-void print_int(va_list arguments)
-{
-	printf("%d", va_arg(arguments, int));
 }
 /**
  * print_all - prints anything
