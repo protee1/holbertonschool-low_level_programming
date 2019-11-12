881557aa5cbd79699b8b5a86e2961df1f0b25d83
arcewp15 <970@holbertonschool.com>
2019-11-12 22:28:27 +0000

    This function appends text at the end  of a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - creates a file
+ * @filename: name of the file
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on Success or -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int filed, i;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	filed = open(filename, O_CREAT | O_APPEND);
+
+	if (filed == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	for (i = 0 ; text_content[i] != '\0' ; i++)
+		;
+
+	write(filed, text_content, i);
+
+	close(filed);
+	return (1);
+}
