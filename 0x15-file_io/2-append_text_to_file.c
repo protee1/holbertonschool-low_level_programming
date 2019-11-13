df9df0bd26ddeb7d3ff9ee0686b7065607db182d
arcewp15 <970@holbertonschool.com>
2019-11-13 18:58:49 +0000

    Add again the code, This function appends text at the end  of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * append_text_to_file - appends test at the end of a file
  * @filename: name of the file
@@ -41,6 +47,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	close(filed);
-
 	return (1);
 }
