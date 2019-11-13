1cc3b466be2b495effab4e7dd053ad710b80fe9e
arcewp15 <970@holbertonschool.com>
2019-11-13 03:06:36 +0000

    This program copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int ff, ft, r, w;
-	char *buffer;
+	char *buffer = malloc(1024);
 
 	if (argc != 3)
 	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -19,19 +19,14 @@ int main(int argc, char *argv[])
 	ff = open(argv[1], O_RDONLY);
 	if (ff == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from a file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	ft = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	buffer = malloc(1024);
-	while ((r = read(ff, buffer, 1024)) > 0)
+	while ((r = read(ff, buffer, 1024)) != 0)
 	{
-		if (r == -1)
-		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
 		w = write(ft, buffer, r);
 		if (ft == -1 || w != r)
 		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -40,11 +35,15 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
+	if (r == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(ff) == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close fd %d", ff), exit(100);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff), exit(100);
 	}
 	if (close(ft) == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't close fd %d", ft), exit(100);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft), exit(100);
 	}
 	return (0);
 }
