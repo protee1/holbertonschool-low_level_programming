3773df0e135d497113131832ef17c3b2c23ac0db
arcewp15 <970@holbertonschool.com>
2019-11-13 16:59:26 -0500

    Trying to fix the checkers

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,22 +25,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	w = write(filed, text_content, i);
 
-	if (w == -1)
-	{
-		return (-1);
-	}
-
 	close(filed);
 	return (1);
 
 	if (filename == NULL)
-<<<<<<< HEAD
 		return (-1);
-=======
-	{
-		return (-1)
-	}
->>>>>>> f3a4440a9be89d568f6504b7968910402b5a6990
 
 	if (text_content == NULL)
 	{
