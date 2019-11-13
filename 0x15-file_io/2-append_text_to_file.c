6c51926bf95cc644d205e8fab4966ee831f26a75
arcewp15 <970@holbertonschool.com>
2019-11-13 16:50:38 -0500

    Add multiple changes of the condition, change every line of the code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,9 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	filed = open(filename, O_WRONLY | O_APPEND, 0600);
 
 	if (filed == -1)
-	{
 		return (-1);
-	}
 
 	w = write(filed, text_content, i);
 
@@ -35,15 +33,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(filed);
 	return (1);
 
+	if (filename == NULL)
+		return (-1);
+
 	if (text_content == NULL)
 	{
-		if (filename == NULL)
+		if (filename != NULL)
 		{
-			return (-1);
+			return (1);
 		}
 		else
 		{
-			return (1);
+			return (-1);
 		}
 	}
 }
