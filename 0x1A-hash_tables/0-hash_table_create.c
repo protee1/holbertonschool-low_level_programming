80af48d92ab0ff4491e32992b79ebbc6017d0e70
arcewp15 <970@holbertonschool.com>
2020-01-18 18:57:38 -0500

    Go through the array, This function creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
+	unsigned int i = 0;
 
 	if (size != 0)
 	{
 		new_hash_table = malloc(sizeof(hash_table_t));
 		new_hash_table->array = malloc(sizeof(hash_table_t *) * size);
 		new_hash_table->size = size;
+
+		while (i < size)
+		{
+			new_hash_table->array[i] = NULL;
+			i++;
+		}
+		return (new_hash_table);
 
 		if (new_hash_table == NULL && new_hash_table->array == NULL)
 		{
