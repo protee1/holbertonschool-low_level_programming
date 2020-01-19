6509debcb52f82b43b57d03e62a2dd1f7037f238
arcewp15 <970@holbertonschool.com>
2020-01-19 17:37:29 -0500

    Testing, Back to the beginning AGAIN, just change a condition of a cicle, This function retrieves a value associated with a key in a hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,16 +21,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	new_node = ht->array[index];
 
-	while (new_node != NULL)
+	while (ht->array[index] != NULL)
 	{
-		if (ht->array[index] != NULL)
+		if (strcmp(ht->array[index]->key, key) == 0)
 		{
-			if (strcmp(ht->array[index]->key, key) == 0)
-			{
-				return (new_node->value);
-			}
-			new_node = new_node->next;
+			return (new_node->value);
 		}
+		new_node = new_node->next;
 	}
 	return (NULL);
 }
