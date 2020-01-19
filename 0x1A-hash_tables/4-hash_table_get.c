45ab7e07bb6456b407ddb9bda24113e93c44714f
arcewp15 <970@holbertonschool.com>
2020-01-19 17:43:20 -0500

    Testing, Back to the beginning AGAIN, just change again the variable, This function retrieves a value associated with a key in a hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,7 +21,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	new_node = ht->array[index];
 
-	while (new_node != NULL)
+	while (ht->array[index] != NULL)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 		{
