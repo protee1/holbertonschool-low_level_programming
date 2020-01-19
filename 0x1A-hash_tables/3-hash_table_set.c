b829ea7723a0b39ac9d41a8e92d1aaff6f0c0c64
arcewp15 <970@holbertonschool.com>
2020-01-18 21:22:38 -0500

    Adding conditions, This function adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,33 +9,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned int index;
-        hash_node_t *new_node;
+	unsigned int index;
+	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || key == '\0')
 		return (0);
 
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if(ht->array[index] == NULL)
+	if (ht->array[index] == NULL)
 	{
-		new_node = malloc(sizeof(hash_node_t));
-
-		if (new_node == NULL)
-			return(0);
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
 		new_node->next = NULL;
 	}
 	else
 	{
-		new_node = malloc(sizeof(hash_node_t));
-
-		if (new_node == NULL)
-			return(0);
-
-		new_node->key = strdup(key);
-		new_node->value = strdup(value);
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
